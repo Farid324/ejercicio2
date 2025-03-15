7a43f5ff90fbfc1895d60755ec41be60d19a3d6b
Grissell Coca <grissellcadima@gmail.com>
2025-03-15 19:57:30 -0400

    Agregada la función de exponenciacion

--- aritmetica.py
+++ aritmetica.py
@@ -12,6 +12,23 @@ class Aritmetica:
         
         print(f"Resultado final: {total}")
 
+    
+    
+    
+    #Grissell Ingrid Coca Cadima
+    def exponenciacion(self, base, exponente):
+        resultado = 1
+        while exponente > 0:
+            if exponente % 2 == 1:
+                resultado *= base
+            base *= base 
+            exponente //=2
+        return resultado
+    
+        
+
+
+
 def menu():
     aritmetica = Aritmetica()
     
@@ -21,13 +38,20 @@ def menu():
         print("2. Resta")
         print("3. Multiplicación")
         print("4. División")
-        print("5. Salir")
+        print("5. Exponenciación")
+        print("6. Salir")
         
-        opcion = input("Seleccione una operación (1-5): ")
+        opcion = input("Seleccione una operación (1-6): ")
         
         if opcion == "1":
             aritmetica.suma()
         elif opcion == "5":
+            print("---------Exponenciación---------")
+            base = float(input("Ingrese el número de la base: "))
+            exponente = int(input("Ingrese el número del exponente: "))
+            resultado = aritmetica.exponenciacion(base, exponente)
+            print(f"Resultado: {resultado}")
+        elif opcion == "6":
             print("Saliendo del programa...")
             break
         else:
